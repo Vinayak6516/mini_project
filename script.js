e727a727cabb003ba48c436246df2f291aa31312
Vinayak6516 <vvinu6516@gmail.com>
2024-10-14 10:58:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var myButton=document.getElementById('button1');
+function func1() {
+  window.location.href="https://static.toiimg.com/thumb/msid-59355141,width-1280,height-720,imgsize-293364,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"
+}
