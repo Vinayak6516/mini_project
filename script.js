fe53f9697acac8e55474a73a1923949319133e24
Vinayak6516 <vvinu6516@gmail.com>
2024-11-15 16:44:42 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ var myButton=document.getElementById('button1');
 function func1() {
   window.location.href="https://static.toiimg.com/thumb/msid-59355141,width-1280,height-720,imgsize-293364,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"
 }
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 200,
+//     reset: true
+});
+sr.reveal('.head',{delay:400});
+sr.reveal('.nav',{delay:800});
